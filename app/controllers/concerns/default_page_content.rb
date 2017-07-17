8b959d85b02184b2c333146539181b330a5ab6d0
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 14:48:44 +0000

    Implemented Bootsrap and other things

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -5,7 +5,7 @@ module DefaultPageContent
      before_filter :set_page_defaults
   end
 
- def set_page_defaults
+  def set_page_defaults
     @page_title = "Devcamp Portfolio | My Portfolio Website"
     @seo_keywords = "Quentin Douglas portfolio"
   end
