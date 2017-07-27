5acbfe7d982040d306e85ec266c4755a30aeac61
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 17:01:19 +0000

    Implementd partials for blog, font awesome, and pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,11 +2,11 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
-  
-  # GET /blogs 
+
+  # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.special_blogs
+    @blogs = Blog.page(params[:page]).per(5)
     @page_title = "My Portfolio Blog"
   end
 
@@ -14,6 +14,7 @@ class BlogsController < ApplicationController
   # GET /blogs/1.json
   def show
     @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
@@ -32,7 +33,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
       else
         format.html { render :new }
       end
@@ -56,18 +57,18 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
       format.json { head :no_content }
     end
   end
-  
+
   def toggle_status
     if @blog.draft?
-    @blog.published!
+      @blog.published!
     elsif @blog.published?
       @blog.draft!
     end
-    
+        
     redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
