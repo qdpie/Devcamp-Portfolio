4ff0fd59cc0359c42ce60a065253811f67ce2088
Jordan Hudgens <jordan@devcamp.com>
2017-08-01 15:48:48 +0000

    Implemented persistent position updates for drag and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,16 +1,24 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
-  def index 
+  def index
     @portfolio_items = Portfolio.by_position
   end
-  
+
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+
+    render nothing: true
+  end
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
@@ -30,12 +38,10 @@ class PortfoliosController < ApplicationController
 
   def edit
   end
-  
+
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-    s
     respond_to do |format|
-      if  @portfolio_item.update(portfolio_params)
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
@@ -47,26 +53,23 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    # Perform the lookup 
-    @portfolio_item = Portfolio.find(params[:id])
-    
     # Destroy/delete the record
-     @portfolio_item.destroy
-     
-     # Redirect
+    @portfolio_item.destroy
+
+    # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+
   private
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, 
-                                      :subtitle, 
-                                      :body, 
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
                                       technologies_attributes: [:name]
-                                      )
+                                     )
   end
 
   def set_portfolio_item
